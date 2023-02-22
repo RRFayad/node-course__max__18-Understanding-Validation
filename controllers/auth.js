bc9db44740c561798c56e6f196060222cab5e690
RRFayad <renan.r.fayad@gmail.com>
2023-02-22 15:40:09 -0300

    298. Keeping User Input (UX purposes)

--- controllers/auth.js
+++ controllers/auth.js
@@ -28,6 +28,7 @@ exports.getLogin = (req, res, next) => {
     path: "/login",
     pageTitle: "Login",
     errorMessage: message,
+    oldInput: { email: "", password: "" },
   });
 };
 
@@ -42,6 +43,7 @@ exports.getSignup = (req, res, next) => {
     path: "/signup",
     pageTitle: "Signup",
     errorMessage: message,
+    oldInput: { email: "", password: "", confirmPassword: "" },
   });
 };
 
@@ -55,6 +57,7 @@ exports.postLogin = (req, res, next) => {
       path: "/signup",
       pageTitle: "Signup",
       errorMessage: errors.array()[0].msg,
+      oldInput: { email, password },
     });
   }
 
@@ -88,6 +91,7 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
@@ -97,6 +101,7 @@ exports.postSignup = (req, res, next) => {
         path: "/signup",
         pageTitle: "Signup",
         errorMessage: errors.array()[0].msg,
+        oldInput: { email, password, confirmPassword },
       });
   }
 
